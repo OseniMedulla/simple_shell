c58b871e006f05a84529dd2dd29b0486d3cca834
OseniMedulla <osenimedulla01@gmail.com>
2022-05-20 15:06:07 -0700

    header

--- shell.h
+++ shell.h
@@ -10,73 +10,95 @@
 #include <sys/wait.h>
 #include <limits.h>
 #include <signal.h>
+#include <stddef.h>
+#include <errno.h>
+#include <fcntl.h>
+
+#include "macros.h" /* for msg help and prompt */
+
 
 /**
- * struct builtins - struct for the builtin functions
- * @name: name of builtin command
- * @f: function for corresponding builtin
+ * struct info- struct for the program's data
+ * @program_name: the name of the executable
+ * @input_line: pointer to the input read for _getline
+ * @command_name: pointer to the first command typed by the user
+ * @exec_counter: number of excecuted comands
+ * @file_descriptor: file descriptor to the input of commands
+ * @tokens: pointer to array of tokenized input
+ * @env: copy of the environ
+ * @alias_list: array of pointers with aliases.
+ */
+typedef struct info
+{
+	char *program_name;
+	char *input_line;
+	char *command_name;
+	int exec_counter;
+	int file_descriptor;
+	char **tokens;
+	char **env;
+	char **alias_list;
+} data_of_program;
+
+/**
+ * struct builtins - struct for the builtins
+ * @builtin: the name of the builtin
+ * @function: the associated function to be called for each builtin
  */
 typedef struct builtins
 {
-	char *name;
-	void (*f)(vars_t *);
-} builtins_t;
+	char *builtin;
+	int (*function)(data_of_program *data);
+} builtins;
 
 
+void inicialize_data(data_of_program *data, int arc, char *argv[], char **env);
+void sisifo(char *prompt, data_of_program *data);
+void handle_ctrl_c(int opr UNUSED);
+int _getline(data_of_program *data);
+int check_logic_ops(char *array_commands[], int i, char array_operators[]);
+void expand_variables(data_of_program *data);
+void expand_alias(data_of_program *data);
+int buffer_add(char *buffer, char *str_to_add);
+void tokenize(data_of_program *data);
 
-/**
- * struct variables - variables
- * @av: command line arguments
- * @buffer: buffer of command
- * @env: environment variables
- * @count: count of commands entered
- * @argv: arguments at opening of shell
- * @status: exit status
- * @commands: commands to execute
- */
-typedef struct variables
-{
-	char **av;
-	char *buffer;
-	char **env;
-	size_t count;
-	char **argv;
-	int status;
-	char **commands;
-} vars_t;
+char *_strtok(char *line, char *delim);
+int execute(data_of_program *data);
+int builtins_list(data_of_program *data);
+char **tokenize_path(data_of_program *data);
+int find_program(data_of_program *data);
+void free_array_of_pointers(char **directories);
+void free_recurrent_data(data_of_program *data);
 
-char **make_env(char **env);
-void free_env(char **env);
+void free_all_data(data_of_program *data);
+int builtin_exit(data_of_program *data);
+int builtin_cd(data_of_program *data);
+int set_work_directory(data_of_program *data, char *new_dir);
+int builtin_help(data_of_program *data);
+int builtin_alias(data_of_program *data);
 
-ssize_t _puts(char *str);
-char *_strdup(char *strtodup);
-int _strcmpr(char *strcmp1, char *strcmp2);
-char *_strcat(char *strc1, char *strc2);
-unsigned int _strlen(char *str);
+int builtin_env(data_of_program *data);
+int builtin_set_env(data_of_program *data);
+int builtin_unset_env(data_of_program *data);
+char *env_get_key(char *name, data_of_program *data);
+int env_set_key(char *key, char *value, data_of_program *data);
+int env_remove_key(char *key, data_of_program *data);
+void print_environ(data_of_program *data);
+int _print(char *string);
 
-char **tokenize(char *buffer, char *delimiter);
-char **_realloc(char **ptr, size_t *size);
-char *new_strtok(char *str, const char *delim);
+int _printe(char *string);
+int _print_error(int errorcode, data_of_program *data);
+int str_length(char *string);
+char *str_duplicate(char *string);
+int str_compare(char *string1, char *string2, int number);
+char *str_concat(char *string1, char *string2);
 
-void (*check_for_builtins(vars_t *vars))(vars_t *vars);
-void new_exit(vars_t *vars);
-void _env(vars_t *vars);
-void new_setenv(vars_t *vars);
-void new_unsetenv(vars_t *vars);
-
-void add_key(vars_t *vars);
-char **find_key(char **env, char *key);
-char *add_value(char *key, char *value);
-int _atoi(char *str);
-
-void check_for_path(vars_t *vars);
-int path_execute(char *command, vars_t *vars);
-char *find_path(char **env);
-int execute_cwd(vars_t *vars);
-int check_for_dir(char *str);
-
-void print_error(vars_t *vars, char *msg);
-void _puts2(char *str);
-char *_uitoa(unsigned int count);
+void str_reverse(char *string);
+void long_to_string(long number, char *string, int base);
+int _atoi(char *s);
+int count_characters(char *string, char *character);
+int print_alias(data_of_program *data, char *alias);
+char *get_alias(data_of_program *data, char *alias);
+int set_alias(char *alias_string, data_of_program *data);
 
 #endif
