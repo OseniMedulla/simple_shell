81b174f8687c8eed229653f8770d765c843954e5
erhurunp <erhurunp@gmail.com>
2022-05-21 09:19:01 +0100

    Edited expansions.c

--- expansions.c
+++ expansions.c
@@ -93,3 +93,24 @@ void expand_alias(data_of_program *data)
 		data->input_line = str_duplicate(line);
 	}
 }
+
+/**
+ * buffer_add - append string at end of the buffer
+ * @buffer: buffer to be filled
+ * @str_to_add: string to be copied in the buffer
+ * Return: nothing, but sets errno.
+ */
+
+int
+buffer_add(char *buffer, char *str_to_add)
+{
+	int length, i;
+
+	length = str_length(buffer);
+	for (i = 0; str_to_add[i]; i++)
+	{
+		buffer[length + i] = str_to_add[i];
+	}
+	buffer[length + i] = '\0';
+	return (length + i);
+}
