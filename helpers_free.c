00bb7f970edb70428fbfd4f59d21da4ee8975cae
erhurunp <erhurunp@gmail.com>
2022-05-21 09:38:07 +0100

    edited file

--- helpers_free.c
+++ helpers_free.c
@@ -37,3 +37,24 @@ void free_all_data(data_of_program *data)
 	free_array_of_pointers(data->env);
 	free_array_of_pointers(data->alias_list);
 }
+
+/**
+ * free_array_of_pointers - frees each pointer of an array of pointers and the
+ * array too
+ * @array: array of pointers
+ * Return: nothing
+ */
+
+void free_array_of_pointers(char **array)
+{
+	int i;
+
+	if (array != NULL)
+	{
+		for (i = 0; array[i]; i++)
+			free(array[i]);
+
+		free(array);
+		array = NULL;
+    }
+}
