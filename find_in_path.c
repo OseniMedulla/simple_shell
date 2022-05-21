3c05ee296967e4d5a7986ad01b82813eb0bb4c0d
erhurunp <erhurunp@gmail.com>
2022-05-21 09:24:39 +0100

    edited file

--- find_in_path.c
+++ find_in_path.c
@@ -48,3 +48,73 @@ int find_program(data_of_program *data)
 	free_array_of_pointers(directories);
 	return (ret_code);
 }
+
+/**
+* tokenize_path - tokenize the path in directories
+* @data: a pointer to the program's data
+* Return: array of path directories
+*/
+
+char **tokenize_path(data_of_program *data)
+{
+	int i = 0;
+	int counter_directories = 2;
+	char **tokens = NULL;
+	char *PATH;
+
+	/* get the PATH value*/
+	PATH = env_get_key("PATH", data);
+	if ((PATH == NULL) || PATH[0] == '\0')
+	{ /*path not found*/
+		return (NULL);
+	}
+
+	PATH = str_duplicate(PATH);
+
+	/* find the number of directories in the PATH */
+	for (i = 0; PATH[i]; i++)
+	{
+		if (PATH[i] == ':')
+			counter_directories++;
+	}
+
+	/* reserve space for the array of pointers */
+	tokens = malloc(sizeof(char *) * counter_directories);
+
+	/*tokenize and duplicate each token of path*/
+	i = 0;
+	tokens[i] = str_duplicate(_strtok(PATH, ":"));
+	while (tokens[i++])
+	{
+		tokens[i] = str_duplicate(_strtok(NULL, ":"));
+	}
+
+	free(PATH);
+	PATH = NULL;
+	return (tokens);
+}
+
+/**
+ * check_file - checks if exists a file, if it is not a dairectory and
+ * if it has excecution permisions for permisions.
+ * @full_path: pointer to the full file name
+ * Return: 0 on success, or error code if it exists.
+ */
+
+int check_file(char *full_path)
+{
+	struct stat sb;
+
+	if (stat(full_path, &sb) != -1)
+	{
+		if (S_ISDIR(sb.st_mode) || access(full_path, X_OK))
+		{
+			errno = 126;
+			return (126);
+		}
+		return (0);
+	}
+	/*if not exist the file*/
+	errno = 127;
+	return (127);
+}
